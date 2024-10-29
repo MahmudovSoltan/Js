5be2e44a81be62fc3d654533a3885da26087823a
MahmudovSoltan <mahmudovsoltan477@gmail.com>
2024-10-29 17:20:49 +0400

    new create js repostery

---
+++ app.js
@@ -0,0 +1,37 @@
+//  Birinci tapsiriq
+
+
+var childerenCount = 5
+var prtnaresName = "Hesen"
+var location = "Azeribaijan"
+var jobTitle = "Devoloper"
+
+// console.log(childerenCount,prtnaresName,location,jobTitle);
+
+
+// Ikinci tapsiriq
+
+
+var myName = "Soltan"
+var myWifeName = "Aysel"
+var myChildren = 2
+// console.log("Menim adim "+myName,"ve menim heyat yoldasim "+myWifeName,"ve bizim ovladlarimizin sayi "+myChildren);
+
+//Ücuncu tapşırıq
+
+var myBirth = 1998
+var furnutureYear = 2030
+var resualtAge = furnutureYear - myBirth
+
+// console.log("men "+furnutureYear+" bu ilde iyulda "+resualtAge+" bu yasda olacam "+"yada iyuldan qabaq olsa bir yas balaca olacam");
+
+// Dorduncu tapsiriq 
+
+
+var lifetimeSupply = 3
+var myCurrentAge = 26
+var myTotalAge = 70
+var resaltLifetimeSupply = lifetimeSupply*(365*(myTotalAge-myCurrentAge))
+
+// console.log(resaltLifetimeSupply);
+
