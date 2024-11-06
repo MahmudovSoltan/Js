930220d0fbd8c3f0f03e46c56b2a2e1910255cc7
MahmudovSoltan <mahmudovsoltan477@gmail.com>
2024-11-06 11:16:37 +0400

    js monday exercise

--- app.js
+++ app.js
@@ -93,64 +93,129 @@
 
 // QUESTION GAME HOME WORK
 
-let areYouReady = confirm("Sual oyununa hazirsan!");
+// let areYouReady = confirm("Sual oyununa hazirsan!");
 
-if (areYouReady === true) {
-  let ruler = alert("Cavablari boyuk herifle yazin ");
-  let q1 = prompt(
-    "HTML-də səhifə başlığını hansı etiket ilə təyin edirik? A) <title>B) <head>C) <header>D) <heading></heading>"
-  );
-  let q2 = prompt(
-    "CSS-də elementin rəngini dəyişmək üçün hansı xassə istifadə olunur? A) background-color B) text-style C) color  font-color "
-  );
-  let q3 = prompt(
-    "HTML-də siyahı (list) yaratmaq üçün hansı etiketlərdən istifadə olunur? A) <list> və <li> B) <ul> və <li> C) <ol> və <list>  <ul> və <ol>"
-  );
-  let q4 = prompt(
-    "CSS-də font ölçüsünü dəyişmək üçün hansı xassə istifadə olunur? A) font-style B) text-size C) font-size D) size"
-  );
-  let q5 = prompt(
-    "HTML-də bir şəkil əlavə etmək üçün hansı atribut istifadə olunur? A) alt B) src C) href D) link"
-  );
-  let correctAnswer1 = "A";
-  let correctAnswer2 = "B";
-  let correctAnswer3 = "C";
-  let correctAnswer4 = "C";
-  let correctAnswer5 = "B";
+// if (areYouReady === true) {
+//   let ruler = alert("Cavablari boyuk herifle yazin ");
+//   let q1 = prompt(
+//     "HTML-də səhifə başlığını hansı etiket ilə təyin edirik? A) <title>B) <head>C) <header>D) <heading></heading>"
+//   );
+//   let q2 = prompt(
+//     "CSS-də elementin rəngini dəyişmək üçün hansı xassə istifadə olunur? A) background-color B) text-style C) color  font-color "
+//   );
+//   let q3 = prompt(
+//     "HTML-də siyahı (list) yaratmaq üçün hansı etiketlərdən istifadə olunur? A) <list> və <li> B) <ul> və <li> C) <ol> və <list>  <ul> və <ol>"
+//   );
+//   let q4 = prompt(
+//     "CSS-də font ölçüsünü dəyişmək üçün hansı xassə istifadə olunur? A) font-style B) text-size C) font-size D) size"
+//   );
+//   let q5 = prompt(
+//     "HTML-də bir şəkil əlavə etmək üçün hansı atribut istifadə olunur? A) alt B) src C) href D) link"
+//   );
+//   let correctAnswer1 = "A";
+//   let correctAnswer2 = "B";
+//   let correctAnswer3 = "C";
+//   let correctAnswer4 = "C";
+//   let correctAnswer5 = "B";
   
-  let question1 =0
-  let question2 =0
-  let question3 =0
-  let question4 =0
-  let question5 =0
+//   let question1 =0
+//   let question2 =0
+//   let question3 =0
+//   let question4 =0
+//   let question5 =0
 
-  if (q1==correctAnswer1) {
-    question1 = 1
-    console.log("question1",question1);
+//   if (q1==correctAnswer1) {
+//     question1 = 1
+//     console.log("question1",question1);
     
-  }else{
-    question1=0
-  }
-  if (q2==correctAnswer2) {
-    question2=1
-  }else{
-    question2=0
-  }
-  if (q3==correctAnswer3) {
-    question3=1
-  }else{
-    question3=0
-  }
-  if (q4==correctAnswer4) {
-    question4=1
-  }else{
-    question4=0
-  }
-  if (q5==correctAnswer5) {
-    question5=1
-  }else{
-    question5=0
-  }
-  let totalCorrectAnswer=(question1+question2+question3+question4+question5)
-  const netice = alert(" duz cavablarin sayi "+totalCorrectAnswer)
-}
+//   }else{
+//     question1=0
+//   }
+//   if (q2==correctAnswer2) {
+//     question2=1
+//   }else{
+//     question2=0
+//   }
+//   if (q3==correctAnswer3) {
+//     question3=1
+//   }else{
+//     question3=0
+//   }
+//   if (q4==correctAnswer4) {
+//     question4=1
+//   }else{
+//     question4=0
+//   }
+//   if (q5==correctAnswer5) {
+//     question5=1
+//   }else{
+//     question5=0
+//   }
+//   let totalCorrectAnswer=(question1+question2+question3+question4+question5)
+//   const netice = alert(" duz cavablarin sayi "+totalCorrectAnswer)
+// }
+
+
+
+///BIRINCI GUNUN SINIF TAPSIRIQLARI 
+
+//BIRNICI TAPSIRIQ
+var array1 = [1, 2, 3];
+var array2 = [2, 30, 1];
+
+var array3 = array1.concat(array2)
+// console.log(array3);
+// output: [1, 2, 3, 2, 30, 1]
+
+//IKINCI TAPSIRIQ
+
+var list1 = [7, 9, 0, -2]
+var list2 = [[1],3]
+var resultList1=list1.slice(0,1)
+var resultList2 = list2.slice(0,1)
+// console.log(resultList1);
+// console.log(resultList2);
+// Expected Output :
+// 7
+// [1]
+
+//UCNCU TAPSIRIQ
+var list = [9,9,9,4,6,2]
+// result :"9-9-9-4-6-2"
+// var listJoin = list.join("-")
+// console.log(listJoin); belede olur
+// console.log(list.join("-")); belede 
+
+
+//DORDUNCU TAPSIRIQ 
+var student_list =["Marry","Ali","Sophia","John"]
+var studentName = student_list.indexOf("John")
+// console.log(studentName);
+var result = `${student_list[studentName]} is a student`
+// console.log(result);
+// result: "John is a student"
+
+//BESINCI TAPSIRIQ 
+let array = [1, 2, 3, 4, 5];
+// Output: [5, 4, 3, 2, 1]
+let arr= array.reverse()
+// console.log(arr);
+
+// ALTINCI TAPSIRIQ 
+
+let arrayFlat = [[1, 2], [3, 4], [5, 6]];
+// Output: [1, 2, 3, 4, 5, 6]
+let arrayFlat2 = arrayFlat.flat(1)
+// console.log(arrayFlat2);
+
+//YEDDINCI TAPSIRIQ 
+var list = [1,2,3,4,5,2]
+// result: 2
+var listSlice = list.splice(1,1)
+// console.log(listSlice);
+
+
+
+
+
+
