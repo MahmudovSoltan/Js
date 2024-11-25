e514d381fb70e2e027c483ba49c1e6432b677db4
Soltan <mahmudovsoltan477@gmail.com>
2024-11-25 16:40:36 +0400

    guessGame

--- JavaScriptHomeworks/guessWord.js
+++ JavaScriptHomeworks/guessWord.js
@@ -5,18 +5,28 @@ let guessWord = ""
 for (let i = 0; i < words[randomNumber].length; i++) {
       guessWord += "_" 
 }
-
 const newGuessWordArr = guessWord.split('')
 guessWord.split("")
-
+let chance = 4
 while(newGuessWordArr.includes("_")){
+if (chance == 0) {
+      alert("Sizin cavab sansiniz itdi")
+      if (chance == 0) {
+       newGuessWordArr=""  
+      }
+} else {
       alert("Hazırkı söz: " + newGuessWordArr.join("") + "\nSözü təxmin etməyə çalışın!");
-      let userWord = prompt("herifi daxil et")
+      let userWord = prompt("herifi daxil et"+"\n"+chance+"defe sehv etme sansiniz var!")
       const findIndex = hiddenWord.indexOf(userWord)
       if (findIndex !==-1) {
             newGuessWordArr[findIndex] = userWord   
       }
-    console.log(guessWord);
+      if (findIndex == -1) {
+            alert("Yazdiginiz herif duz deyil")
+            chance-=1  
+      } 
+}
+
     
 }
 alert("Tapdiginiz soz " + newGuessWordArr.join(""))
