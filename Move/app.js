fda15bf0312f843f3f9b619508010936e6e3dd71
Soltan <mahmudovsoltan477@gmail.com>
2025-01-02 23:31:45 +0400

    add new task coderwors

---
+++ Move/app.js
@@ -0,0 +1,18 @@
+const inputEl = document.querySelector("input");
+const mainDiv = document.querySelector("#mainDiv");
+const btn = document.querySelector("button");
+const image = document.querySelector("img");
+const apiKey = "e76951c6";
+const url = `https://www.omdbapi.com/?t=Batman&apikey=${apiKey}`;
+async function getdata(url) {
+  try {
+    const data = await fetch(url);
+    const data2 = await data.json();
+    console.log(data2);
+    image.src = data2.Poster;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getdata(url);
